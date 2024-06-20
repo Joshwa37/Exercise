de6ae1771018d359eaadb2eb4e198d6f62dddb97
Joshwa37 <146755994+Joshwa37@users.noreply.github.com>
2024-06-20 18:57:12 +0530

    Add files via upload

---
+++ esmodule/module.js
@@ -0,0 +1,4 @@
+export const PI =3.14;
+export function circumference(r){
+    return 2*PI*r;
+}
---
+++ esmodule/module1.js
@@ -0,0 +1,3 @@
+import {PI,circumference} from './module.js';
+const val=circumference(10);
+console.log(val);
